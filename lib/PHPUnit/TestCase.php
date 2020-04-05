f1b92319ae9b57d0372c40a5a5a0adfe164d05e5
Daniel Leech <dan.t.leech@gmail.com>
2020-04-05 11:40:25 +0100

    Change trait declaration order

--- lib/PHPUnit/TestCase.php
+++ lib/PHPUnit/TestCase.php
@@ -1,21 +1,5 @@
 <?php
 
-namespace Phpactor\TestUtils\PHPUnit {
-
-    use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
-    use PHPUnit\Framework\TestCase as PhpUnitTestCase;
-    use Prophecy\PhpUnit\ProphecyTrait;
-
-    class TestCase extends PhpUnitTestCase
-    {
-        use ArraySubsetAsserts;
-        use ProphecyTrait;
-    }
-}
-
-// add stubs for PHPUnit < 8
-// when all packages are using PHPUnit 9 these can be removed
-
 namespace DMS\PHPUnitExtensions\ArraySubset{
     if (!trait_exists(ArraySubsetAsserts::class)) {
         trait ArraySubsetAsserts
@@ -29,5 +13,18 @@ namespace Prophecy\PhpUnit {
         trait ProphecyTrait
         {
         }
+    }
+}
+
+namespace Phpactor\TestUtils\PHPUnit {
+
+    use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+    use PHPUnit\Framework\TestCase as PhpUnitTestCase;
+    use Prophecy\PhpUnit\ProphecyTrait;
+
+    class TestCase extends PhpUnitTestCase
+    {
+        use ArraySubsetAsserts;
+        use ProphecyTrait;
     }
 }
